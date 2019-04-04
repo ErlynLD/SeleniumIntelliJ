b00fa19487e72d4c2b0f0645e66512fde8acd6f4
ErlynLD <erlyn-x@hotmail.com>
2019-04-03 23:08:47 -0400

    New Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,4 +4,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Here other line. Just using Git");
     }
+
+    /*
+    *  Comment from otroParaMerge
+    * */
 }
